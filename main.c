2b6ad469443c630de20898a92e8d8a6f83603dba
wmatt <matt@weingarten.org>
2021-04-25 12:27:46 +0200

    Changes rows/features

--- main.c
+++ main.c
@@ -21,42 +21,42 @@ int main(int argc, char **argv) {
 	// printf("columns: %d\n",count_columns(file));
 	// get_table(file,count_rows(file),count_columns(file));
 	//
-	int rows = 8; //numsamples
-	int cols = 3;  // numfeatures?
+	int rows = 16; //numsamples
+	int cols = 4;  // numfeatures?
 
 
 	printf("Size : %d\n", rows * cols );
 
 	uint32_t * src = (uint32_t* ) malloc(rows * cols * sizeof(uint32_t));
+	uint32_t * dest = (uint32_t* ) malloc(rows * cols * sizeof(uint32_t));
+	uint32_t* q1_dest = (uint32_t*) malloc(rows * sizeof(uint32_t));
 
 	uint32_t count = 0;
 	for(int i = 0; i < rows;++i){
 		for(int j = 0; j < cols;++j){
 			src[i * cols + j] = count %5;
-			printf("src[%d] = %d \n",i*cols + j,count);
+			printf("src[%d] = %d \n",i*cols + j,count %5);
 			count++;
 		}	
 	}
 	PRINT_MALLOC(src,rows,cols);
 	PRINT_MALLOC_B(src,rows,cols);
-	uint32_t * dest = (uint32_t* ) malloc(rows * cols * sizeof(uint32_t));
+
 	weave_samples_simple(dest,src,rows,cols);
-	PRINT_MALLOC(dest,rows,cols);
+	// PRINT_MALLOC(dest,rows,cols);
 	LINE;LINE;
 	PRINT_MALLOC_B(dest,rows,cols);
 
 
 	//straightforward q1
-	uint32_t* q1_dest = (uint32_t*) malloc(rows * sizeof(uint32_t));
-	q1(src,q1_dest,rows,cols);
-	uint64_t sum = q2(src,rows,cols);
 
+	q1(src,q1_dest,rows,cols);
 	printf("Q1 result:\n");
 	PRINT_MALLOC(q1_dest,rows,1);
 	printf("Q2 result:\n");
-	printf("%d\n",sum);
-	
+	printf("%d\n",q2(src,rows,cols));
 	free(src);
 	free(dest);
+	free(q1_dest);
 	return 0;
 }
