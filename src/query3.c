b17c0138d0c9ec294b0a0e6f38d8cbc416299d63
domihaener <38081965+domihaener@users.noreply.github.com>
2021-05-01 13:52:03 +0200

    query 3 validation version

--- src/query3.c
+++ src/query3.c
@@ -10,6 +10,17 @@
 
 //Straightforward
 
-void q3(void){
-    return;
+// always assuming R.a == first feature of R
+// S.b == second feature of S, S.c == third feature of S
+// dest should be R_rows * S_rows long, 2d array, indexing which combinations of R and S to output
+void q3(uint32_t *dR, uint32_t *dS, uint32_t * dest, int R_rows, int R_cols, int S_rows, int S_cols){
+    for(int i = 0; i < R_rows; i++){
+		for(int j = 0; j < S_rows; j++){
+				if(dR[i * R_cols] % dS[j * S_cols + 1] == dS[j * S_cols + 2]){
+					dest[i * S_rows + j] = 1;
+				} else {
+					dest[i * S_rows + j] = 0;
+				}
+		}
+	}
 }
