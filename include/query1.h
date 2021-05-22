a00e4b4dc3fc55e9b0ee5050f12b3d96132ba532
domihaener <38081965+domihaener@users.noreply.github.com>
2021-05-22 18:10:54 +0200

    added q1 vector weave v2-v5, yet to check timings

--- include/query1.h
+++ include/query1.h
@@ -28,4 +28,11 @@ void q1_parallel_weave_v2(uint32_t * data,uint32_t * results,uint32_t *temps,int
 
 void q1_vector_weave(uint32_t * data,uint32_t * results,uint32_t *temps,int word_size,int block_size,int num_samples, int num_features,int number_entries);
 
+void q1_vector_weave_v2(uint32_t * data,uint32_t * results,uint32_t *temps,int word_size,int block_size,int num_samples, int num_features,int number_entries);
+
+void q1_vector_weave_v3(uint32_t * data,uint32_t * results,uint32_t *temps,int word_size,int block_size,int num_samples, int num_features,int number_entries);
+
+void q1_vector_weave_v4(uint32_t * data,uint32_t * results,uint32_t *temps,int word_size,int block_size,int num_samples, int num_features,int number_entries);
+
+void q1_vector_weave_v5(uint32_t * data,uint32_t * results,uint32_t *temps,int word_size,int block_size,int num_samples, int num_features,int number_entries);
 #endif
