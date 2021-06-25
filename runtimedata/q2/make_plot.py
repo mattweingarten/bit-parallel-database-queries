1dd4ae1b2f0143645a80b70dbb27b0f0c69c09f3
sommemic <sommemic@ethz.ch>
2021-06-25 14:21:26 +0200

    Tweaking q2 plot

--- runtimedata/q2/make_plot.py
+++ runtimedata/q2/make_plot.py
@@ -14,21 +14,30 @@ df = pd.read_csv("query2.csv", sep=';', engine='python')
 
 x = np.linspace(0, len(df['rows']) - 1 , len(df['rows']))
 plt.yscale('log',base=2) 
+ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
 # df.plot()
 # print(len(x))
 # print(len(df['rows']))
 # print(len(df['parallel']))
 # plt.plot(x,df['parallel'])
+
+
 plt.plot(x,df['simple-index'],linestyle="dashed",color="navajowhite",marker="*",label="simplified indexing")
+
 plt.plot(x,df['scalar'],linestyle="dashed",color="plum",marker="+",label="scalar replacement")
+
 plt.plot(x,df['scalar/vec'],linestyle="dashed",color="darksalmon",marker="D",label="scalar & vector accumulation ")
-plt.plot(x,df['unroll'],linestyle="dashed",color="peru",marker="2",label="unroll 4x")
+
+plt.plot(x,df['unroll'],linestyle="dashed",color="steelblue",marker="2",label="unroll 4x")
+
 plt.plot(x,df['unroll/vec'],linestyle="dashed",color="olive",marker="h",label="unroll 4x & vector accumulation")
+
 plt.plot(x,df['vectorized'],linestyle="dashed",color="slategrey",marker="v",label="Fully vectorized")
 
 
-plt.vlines(9,ymin=0.1, ymax=130,linestyles ='dotted')
-#plt.ticklabel_format(style='plain') 
+
+plt.vlines(9,ymin=0.1, ymax=130,linestyles ='dotted', colors={"black"})
+plt.ticklabel_format(style='plain') 
 
 
 # plt.title('Q1 performance\nX times slower than memory bandiwdth')
